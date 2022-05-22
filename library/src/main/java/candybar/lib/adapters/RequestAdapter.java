58386ea8feaef2ae7739f2bebdbb2972afa497e0
Donnnno <31142286+Donnnno@users.noreply.github.com>
2022-05-22 19:53:18 +0200

    Fix wrong icon getting selected in icon request
    https://github.com/zixpo/candybar/commit/3d8662c9c7b06f4327e3fecc208b8214713df516

--- library/src/main/java/candybar/lib/adapters/RequestAdapter.java
+++ library/src/main/java/candybar/lib/adapters/RequestAdapter.java
@@ -391,7 +391,7 @@ public class RequestAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
         public void onClick(View view) {
             int id = view.getId();
             if (id == R.id.container) {
-                int position = mShowPremiumRequest ?
+                int position = mShowPremiumRequest || mShowRegularRequestLimit ?
                         getBindingAdapterPosition() - 1 : getBindingAdapterPosition();
                 if (toggleSelection(position)) {
                     checkbox.toggle();
@@ -403,7 +403,7 @@ public class RequestAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
         public boolean onLongClick(View view) {
             int id = view.getId();
             if (id == R.id.container) {
-                int position = mShowPremiumRequest ?
+                int position = mShowPremiumRequest || mShowRegularRequestLimit ?
                         getBindingAdapterPosition() - 1 : getBindingAdapterPosition();
                 if (toggleSelection(position)) {
                     checkbox.toggle();
