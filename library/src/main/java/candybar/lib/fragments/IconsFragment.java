09aba536341749b42fc0d156127469c4d433af06
Donnnno <31142286+Donnnno@users.noreply.github.com>
2023-05-14 20:50:10 +0200

    Prevent crash when not attached to activity
    https: //github.com/zixpo/candybar/commit/cc42665b8755bc1e0e7b9e0d8a670f46efae2f14
    Co-Authored-By: Stefanie Grunwald <hi@moer.tel>

--- library/src/main/java/candybar/lib/fragments/IconsFragment.java
+++ library/src/main/java/candybar/lib/fragments/IconsFragment.java
@@ -153,7 +153,7 @@ public class IconsFragment extends Fragment {
     }
 
     public void refreshBookmarks() {
-        if (isBookmarksFragment) {
+        if (isBookmarksFragment && isAdded()) {
             mIcons = Database.get(requireActivity()).getBookmarkedIcons(requireActivity());
             mAdapter.setIcons(mIcons);
             setupViewVisibility();
