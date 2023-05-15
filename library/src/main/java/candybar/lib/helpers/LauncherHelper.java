28225068550775a17c7829f131f39bf9dfd0bb99
Donnnno <31142286+Donnnno@users.noreply.github.com>
2023-05-15 22:06:20 +0200

    Add analytics events for OneUI launcher

--- library/src/main/java/candybar/lib/helpers/LauncherHelper.java
+++ library/src/main/java/candybar/lib/helpers/LauncherHelper.java
@@ -905,6 +905,14 @@ public class LauncherHelper {
                 )
                 .positiveText(android.R.string.yes)
                 .onPositive((dialog, which) -> {
+                    CandyBarApplication.getConfiguration().getAnalyticsHandler().logEvent(
+                            "click",
+                            new HashMap<String, Object>() {{
+                                put("section", "apply");
+                                put("action", "manual_open_confirm");
+                                put("launcher", launcherName);
+                            }}
+                    );
                     if (Build.VERSION.SDK_INT > Build.VERSION_CODES.R) {
                         String packageName = "com.samsung.android.themedesigner";
                         try {
@@ -937,6 +945,16 @@ public class LauncherHelper {
                     }
                 })
                 .negativeText(android.R.string.cancel)
+                .onNegative(((dialog, which) -> {
+                    CandyBarApplication.getConfiguration().getAnalyticsHandler().logEvent(
+                            "click",
+                            new HashMap<String, Object>() {{
+                                put("section", "apply");
+                                put("action", "manual_open_cancel");
+                                put("launcher", launcherName);
+                            }}
+                    );
+                }))
                 .show();
     }
 
