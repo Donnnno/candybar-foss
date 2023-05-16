10ac2dcfe4a6f63e01edd2125cf2b2cde61e8115
Donnnno <31142286+Donnnno@users.noreply.github.com>
2023-05-16 23:36:33 +0200

    remove License.java

--- app/src/main/java/com/candybar/dev/activities/MainActivity.java
+++ app/src/main/java/com/candybar/dev/activities/MainActivity.java
@@ -2,8 +2,6 @@ package com.candybar.dev.activities;
 
 import androidx.annotation.NonNull;
 
-import com.candybar.dev.licenses.License;
-
 import candybar.lib.activities.CandyBarMainActivity;
 
 public class MainActivity extends CandyBarMainActivity {
