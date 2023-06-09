d744755018bd0afb9a104136347f71c16d63ebe5
Donnnno <31142286+Donnnno@users.noreply.github.com>
2023-06-09 22:42:48 +0200

    Add support for rendering VectorDrawable
    Co-Authored-By: Sarsa Murmu <sarsamurmu05@gmail.com>

--- library/src/main/java/candybar/lib/utils/CommonDataFetcher.java
+++ library/src/main/java/candybar/lib/utils/CommonDataFetcher.java
@@ -6,12 +6,16 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.graphics.drawable.AdaptiveIconDrawable;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.VectorDrawable;
 import android.os.Build;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,6 +24,10 @@ import androidx.core.content.ContextCompat;
 import com.bumptech.glide.Priority;
 import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.data.DataFetcher;
+import com.danimahardhika.android.helpers.core.utils.LogUtil;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 import candybar.lib.preferences.Preferences;
 import sarsamurmu.adaptiveicon.AdaptiveIcon;
@@ -39,6 +47,8 @@ public class CommonDataFetcher implements DataFetcher<Bitmap> {
             callback.onDataReady(getDrawable(mModel));
         } else if (mModel.startsWith("package://")) {
             callback.onDataReady(getPackage(mModel));
+        } else if (mModel.startsWith("assets://")) {
+            callback.onDataReady(getAsset(mModel));
         }
     }
 
@@ -78,6 +88,15 @@ public class CommonDataFetcher implements DataFetcher<Bitmap> {
         Drawable drawable = ContextCompat.getDrawable(mContext, drawableId);
 
         if (drawable instanceof BitmapDrawable) return ((BitmapDrawable) drawable).getBitmap();
+        if (drawable instanceof LayerDrawable || drawable instanceof VectorDrawable) {
+            final boolean isVector = drawable instanceof VectorDrawable;
+            final int width = isVector ? 256 : drawable.getIntrinsicWidth();
+            final int height = isVector ? 256 : drawable.getIntrinsicHeight();
+            final Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            drawable.setBounds(0, 0, width, height);
+            drawable.draw(new Canvas(bitmap));
+            return bitmap;
+        }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && drawable instanceof AdaptiveIconDrawable) {
             if (Preferences.get(mContext).getIconShape() == -1) {
@@ -93,6 +112,17 @@ public class CommonDataFetcher implements DataFetcher<Bitmap> {
                     .setDrawable((AdaptiveIconDrawable) drawable)
                     .setPath(Preferences.get(mContext).getIconShape())
                     .render();
+        }
+
+        return null;
+    }
+
+    @Nullable
+    private Bitmap getAsset(String uri) {
+        try (InputStream stream = mContext.getAssets().open(uri.replaceFirst("assets://", ""))) {
+            return BitmapFactory.decodeStream(stream);
+        } catch (IOException e) {
+            LogUtil.e(Log.getStackTraceString(e));
         }
 
         return null;
